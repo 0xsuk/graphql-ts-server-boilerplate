c96fe022088b7c6876a942d1d91c157a48436e00
0xsuk <ikeuu53@yahoo.co.jp>
2022-08-12 16:13:40 +0900

    part 7 done

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,23 +1,11 @@
-import {
-  BaseEntity,
-  BeforeInsert,
-  Column,
-  Entity,
-  PrimaryColumn,
-} from "typeorm";
-import { v4 as uuidv4 } from "uuid";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 //https://github.com/typeorm/typeorm/blob/master/docs/decorator-reference.md#column
 @Entity("users") //table name
 export class User extends BaseEntity {
-  @PrimaryColumn("uuid") id: string;
+  @PrimaryGeneratedColumn("uuid") id: string;
 
   @Column("varchar", { length: 255 }) email: string;
 
   @Column("text") password: string;
-
-  @BeforeInsert()
-  addId() {
-    this.id = uuidv4();
-  }
 }
