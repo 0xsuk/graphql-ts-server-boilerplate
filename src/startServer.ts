9138605089e537e42ffb16bc4693965456bdc759
0xsuk <ikeuu53@yahoo.co.jp>
2022-08-12 15:50:43 +0900

    part 6 done

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,21 +1,31 @@
 import { createServer } from "@graphql-yoga/node";
 import { loadSchemaSync } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
-import { join } from "path";
-import { resolvers } from "./resolvers";
 import { createTypeormConn } from "./utils/createTypeormConn";
+import * as fs from "fs";
+import * as path from "path";
+import { makeExecutableSchema, mergeSchemas } from "@graphql-tools/schema";
+import { GraphQLSchema } from "graphql";
 
 export const startServer = async () => {
-  //https://www.graphql-tools.com/docs/migration/migration-from-import
-  const typeDefs = loadSchemaSync(join(__dirname, "schema.graphql"), {
-    loaders: [new GraphQLFileLoader()],
+  const schemas: GraphQLSchema[] = [];
+  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
+  folders.forEach((folder) => {
+    const { resolvers } = require(`./modules/${folder}/resolvers`);
+    const typeDefs = loadSchemaSync(
+      path.join(__dirname, `./modules/${folder}/schema.graphql`),
+      {
+        loaders: [new GraphQLFileLoader()],
+      }
+    );
+
+    schemas.push(makeExecutableSchema({ typeDefs, resolvers }));
   });
 
+  //https://www.graphql-tools.com/docs/migration/migration-from-import
+
   const server = createServer({
-    schema: {
-      typeDefs,
-      resolvers,
-    },
+    schema: mergeSchemas({ schemas }),
     port: process.env.NODE_ENV === "test" ? 0 : 4000, //TODO what is port 0?
   });
 
