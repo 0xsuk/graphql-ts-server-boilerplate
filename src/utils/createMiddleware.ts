20cb02542b642c96aae593e620ba30c7bef5f569
0xsuk <ikeuu53@yahoo.co.jp>
2022-08-19 18:38:16 +0900

    middleware deleted

--- src/utils/createMiddleware.ts
+++
@@ -1,6 +0,0 @@
-import { GraphQLMiddlwareFunc, Resolver } from "../types/graphql-utils";
-
-export const createMiddleware =
-  (middlewareFunc: GraphQLMiddlwareFunc, resolverFunc: Resolver): Resolver =>
-  (parent, args, context, info) =>
-    middlewareFunc(resolverFunc, parent, args, context, info);
