ad713ed9b3d07dc0699d9d2462f796f6b94c8104
0xsuk <ikeuu53@yahoo.co.jp>
2022-08-19 14:38:40 +0900

    graphql-code-generator with contextType support

--- src/scripts/createTypes.ts
+++ src/scripts/createTypes.ts
@@ -1,6 +1,7 @@
 import { codegen } from "@graphql-codegen/core";
 import * as fs from "fs";
 import * as typescriptPlugin from "@graphql-codegen/typescript";
+import * as typescriptResolvers from "@graphql-codegen/typescript-resolvers";
 import { GraphQLSchema, parse, printSchema } from "graphql";
 import { genSchema } from "../utils/genSchema";
 import path = require("path");
@@ -21,9 +22,15 @@ import path = require("path");
       {
         typescript: {}, // Here you can pass configuration to the plugin
       },
+      {
+        typescriptResolvers: {
+          contextType: "../types/graphql-utils.d#Context",
+        }, // Here you can pass configuration to the plugin
+      },
     ],
     pluginMap: {
       typescript: typescriptPlugin,
+      typescriptResolvers: typescriptResolvers,
     },
   });
 
